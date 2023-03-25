c25b42f41077811a027ea0355fecb6d7687f4493
Surinder P Singh <abbas.abbas340@yahoo.com>
2023-03-24 21:11:32 -0400

    loops

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names) {
+const names1 = []
+for (let i = 0; i < names.length; i++) {
+    names1.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+}
+return names1;
+}
+writeCards(names)
+function countDown() {
+var number = 10;
+while (number >= 0){
+    number--;
+    console.log(number);
+}
+}
